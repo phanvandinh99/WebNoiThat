50b1177a03e2a0302f023e7fc64636e763c0e7a6
Phan Định <81685801+phanvandinh99@users.noreply.github.com>
2022-12-19 04:14:18 +0700

    ADD: Add new word

--- ShopingCart/Controllers/OrderController.cs
+++ ShopingCart/Controllers/OrderController.cs
@@ -108,7 +108,7 @@ namespace ShopingCart.Controllers
                     }
                     main += @"</tbody>
 				</table>";
-                    HelpMail.SendEmail(currentUser.Email, "danhminhhm@gmail.com", "danhngoc99", "[Nội Thất Bích Thủy]_Đơn hàng", header + main + footer);
+                    // HelpMail.SendEmail(currentUser.Email, "danhminhhm@gmail.com", "danhngoc99", "[Nội Thất Bích Thủy]_Đơn hàng", header + main + footer);
                     TempData["message"] = "Added";
                     TempData["DataSuccess"] = "Đặt hàng thành công";
 					Session[Common.CommonConstants.SESSION_CART] = null;
